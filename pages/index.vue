4153c0c8bcda651810e7daafa8d10d232613b07f
ngoishi <43993679+ngoishi@users.noreply.github.com>
2020-03-09 23:42:28 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -180,7 +180,7 @@ export default {
       sumInfoOfPatients,
       headerItem: {
         icon: 'mdi-chart-timeline-variant',
-        title: '奈良県内の最新感染動向',
+        title: '奈良県内の最新感染動向 (注意！まだ作成中です。データはすべて見本です。)',
         date: Data.lastUpdate
       },
       newsItems: News.newsItems,
