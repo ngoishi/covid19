339f23422c0c0ba878c606fd6f91aabcc2303d18
ngoishi <43993679+ngoishi@users.noreply.github.com>
2020-03-10 10:30:24 +0900

    Update formatConfirmedCases.ts

--- utils/formatConfirmedCases.ts
+++ utils/formatConfirmedCases.ts
@@ -7,15 +7,15 @@ type DataType = {
       value: number
       children: [
         {
-          attr: '入院中'
+          attr: '入院中等'
           value: number
           children: [
             {
-              attr: '軽症・中等症'
+              attr: '入院中'
               value: number
             },
             {
-              attr: '重症'
+              attr: '入院調整中'
               value: number
             }
           ]
@@ -36,9 +36,9 @@ type DataType = {
 type ConfirmedCasesType = {
   検査実施人数: number
   陽性物数: number
+  入院中等: number
   入院中: number
-  軽症中等症: number
-  重症: number
+  入院調整中: number
   死亡: number
   退院: number
 }
@@ -47,9 +47,9 @@ export default (data: DataType) => {
   const formattedData: ConfirmedCasesType = {
     検査実施人数: data.value,
     陽性物数: data.children[0].value,
-    入院中: data.children[0].children[0].value,
-    軽症中等症: data.children[0].children[0].children[0].value,
-    重症: data.children[0].children[0].children[1].value,
+    入院中等: data.children[0].children[0].value,
+    入院中: data.children[0].children[0].children[0].value,
+    入院調整中: data.children[0].children[0].children[1].value,
     死亡: data.children[0].children[2].value,
     退院: data.children[0].children[1].value
   }
